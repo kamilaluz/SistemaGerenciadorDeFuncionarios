c87d0134754537b53575895000e9690da8552826
Kamila Luz <146234680+kamilaluz@users.noreply.github.com>
2023-11-03 14:20:20 -0300

    Classe Efetivos criada

---
+++ GerenciarFuncionarios/GerenciarFuncionarios/Classes/Efetivos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciarFuncionarios.Classes
+{
+    internal class Efetivos : Funcionarios
+    {
+        public Efetivos(string nome, int numIdentificacao, string dataNascimento, string cargo, double salario, string telefone) : base(nome, numIdentificacao, dataNascimento, cargo, salario, telefone) { }
+
+        public override double ReajustarSalario()
+        {
+            return Salario + (Salario * 0.1);
+        }
+
+        public override double CalcularPPR()
+        {
+            return 2 * Salario;
+        }
+
+        public override void ExibirInformacoes()
+        {
+            Console.WriteLine($"Olá, meu nome é {Nome}, sou {Cargo} no Senai, trabalho como funcionário(a) Efetivo(a).");
+            Console.WriteLine($"Reajuste Salarial: R$ {ReajustarSalario().ToString("C")}");
+            Console.WriteLine($"PPR: R$ {CalcularPPR().ToString("C")}");
+        }
+    }
+}
